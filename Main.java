ac07616ae0ad819c475c15a538623f2f1f23f58f
CousinCarlo <168486107+CousinCarlo@users.noreply.github.com>
2025-03-26 22:43:50 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,7 +5,6 @@
 
 public class Main
 {
-
 	/**
 	 * ALGORITHM:
 	 * - Add total funds to taco stand
@@ -17,10 +16,7 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
-		//DECLARATION + INITIALIZATION SECTION
 		TacoStand.initialize();
-
-		//INPUT + CALCULATION + OUTPUT SECTION
 		TacoStand.addTotalFunds(20);
 		TacoStand.orderSupplies(15);
 
@@ -29,16 +25,12 @@ public class Main
 
 		Main.printWelcome();
 		System.out.println("\n");
-		
 		Main.takeOrder();
-		//call takeOrder more times if you'd like! (once everything works once though!)
-
-		System.out.println("--------CART IS CLOSED---------\n\n" + TacoStand.getStatus());
+		System.out.println("--------CART IS CLOSED---------");
+		System.out.println();
+	System.out.println(TacoStand.getStatus());
 	}
 
-	/**
-	 * Outputs welcome message to start program that user sees
-	 */
 	public static void printWelcome()
 	{
 		UtilityBelt.printCentered(50, "Welcome to MCC Taco Stand!");
@@ -48,37 +40,30 @@ public class Main
 		UtilityBelt.printCentered(50, "┈┈▏┈▂▂▂▂▂┈▕╮▕┈┈");
 		UtilityBelt.printCentered(50, "┈┈▏┈╲▂▂▂╱┈▕╮▕┈┈");
 		UtilityBelt.printCentered(50, "┈┈╲▂▂▂▂▂▂▂▂╲╱┈┈");
-		//ascii art credit:
-		//https://mizbizbby.tumblr.com/post/12937794639/happy-taco-ascii-art-for-taco-thursday
 	}
-	
-	/**
-	 * Prints menu and prompts user for input for kind of taco and number in order. If tacos are available,
-	 * will update total funds and confirm order with user, otherwise error message given
-	 */
-	public static void takeOrder() //TODO: upgrade per documentation
-	{
-		//DECLARATION + INITIALIZATION SECTION
-		int option, numTacosOrdered;
 
-		//INPUT SECTION
+	public static void takeOrder()
+	{
+		int option, numTacosOrdered;
 		TacoStand.printMenu();
 		option = UtilityBelt.readInt("Enter choice> ", 1, 4);
 		numTacosOrdered = UtilityBelt.readInt("Enter number of tacos you want> ", 1, 50);
-
-		//CALCULATION + OUTPUT SECTION
-		TacoStand.updateTotalFunds(option, numTacosOrdered);
-		Main.printConfirmation(numTacosOrdered);
+		if (TacoStand.areTacosAvailable(option, numTacosOrdered)) {
+			TacoStand.updateTotalFunds(option, numTacosOrdered);
+			Main.printConfirmation(numTacosOrdered);
+		} else {
+			System.out.println("We don't have that many tacos, sorry! Try again :(");
+		}
 	}
 
-	/**
-	 * Prints confirmation message that varies based on number of tacos in order
-	 * 
-	 * @param numTacos
-	 */
-	public static void printConfirmation(int numTacos) //TODO: upgrade per documentation
-	{
-		System.out.println("Here you go, buen provecho!");
-		System.out.println("🌮");
+	public static void printConfirmation(int numTacos)
+{
+    System.out.println("Here you go, buen provecho!");
+	for (int i = 0; i < numTacos; i++) {
+		System.out.print("🌮");
+		
 	}
+	System.out.println("");
+	System.out.println("");
+}
 }
